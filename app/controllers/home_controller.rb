6b140eb5c1f32fab400568d353333483b8a8eed5
KielHaym <callum@ezyvet.com>
2017-04-20 15:38:28 +1200

    login screen

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,5 +4,41 @@ class HomeController < ApplicationController
 
   def decktracker
   end
+
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def create
+  	@user = User.new(user_params)
+
+  	respond_to do |format|
+  		if @user.save
+  			format.html { redirect_to_@user, notice: 'User was successfully created.' }
+  			format.json { render actionL 'show', status: :created, lcation: @user }
+  		else
+  			format.html {render action: 'new' }
+  			format.json { render json: @user.errors, status: :unprocessable_entity }
+  		end
+  	end
+  end
+
+  def update
+  	respond_to do |format|
+  		if @user.update(user_params)
+  			format.html {redirect_to Users_url, notice: 'User was successfully updated.' }
+  			format.json { head :no_content }
+  		else
+  			format.html {render action: 'edit' }
+  			format.json {render json: @user.errors, status: :unprocessable_entity }
+  			end
+  		end
+  	end
+
 end
 
